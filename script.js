e3f7317734234f517763967ab90bdf44d26edc3e
Adiksuu <75419729+Adiksuu@users.noreply.github.com>
2022-01-19 16:17:35 +0100

    APP - quizme
    Files for quiz app, look at https://quizme.ml to check this project

---
+++ script.js
@@ -0,0 +1,161 @@
+const quizData = [
+    {
+        question: "Jakich języków używamy do tworzenia stron?",
+        a: "HTML/PHP/JSON",
+        b: "HTML/CSS/JS",
+        c: "CSS/JSON/JAVA",
+        d: "CSS/HTML/JSON",
+        correct: "b",
+    },
+    {
+        question: "Gdzie najczęściej dodajemy arkusze styli?",
+        a: "JSON",
+        b: "HTML",
+        c: "CSS",
+        d: "JS",
+        correct: "c",
+    },
+    {
+        question: "W jakim języku najczęściej nazywamy nazwy klas?",
+        a: "Polskim",
+        b: "Niemieckim",
+        c: "Angielskim",
+        d: "W żadnym z podanych",
+        correct: "c",
+    },
+    {
+        question: "Aby podpiąć JS używamy?",
+        a: "<link rel='stylesheet' href='script.js'>",
+        b: "<script src='script.js'>",
+        c: "<img src='script.js'>",
+        d: "Nie trzeba tego podpiąć!",
+        correct: "b",
+    },
+    {
+        question: "HTML oraz CSS nazywamy językami:",
+        a: "Programowania",
+        b: "Znaczników",
+        c: "Kodowania",
+        d: "Nie ma czegoś takiego jak HTML lub CSS!",
+        correct: "b",
+    },
+    {
+        question: "Najpopularniejszym edytorem kodu jest:",
+        a: "Notatnik",
+        b: "VisualStudioCode (VSC)",
+        c: "Notepad++",
+        d: "Brackets/Atom",
+        correct: "c",
+    },
+    {
+        question: "Margines w CSS określamy za pomocą:",
+        a: "margin: 10px;",
+        b: "padding: 10;",
+        c: "display: flex;",
+        d: "Nie ma czegoś takiego jak margines w CSS",
+        correct: "a",
+    },
+    {
+        question: "Stałe Zmienne w JS możemy zdefiniować jako",
+        a: "var",
+        b: "let",
+        c: "function",
+        d: "const",
+        correct: "d",
+    },
+    {
+        question: "Aktualną wersją HTML jest:",
+        a: "HTML3",
+        b: "HTML4",
+        c: "HTML5",
+        d: "HTML6",
+        correct: "c",
+    },
+    {
+        question: "Aktualną wersją JS jest:",
+        a: "ES6",
+        b: "ES7",
+        c: "ES5",
+        d: "ES4",
+        correct: "a",
+    },
+
+
+];
+
+const startTime = new Date().getTime();
+
+const quiz = document.getElementById('quiz')
+const text = document.getElementById('score')
+const quizLevel = document.getElementById('time')
+const answerEls = document.querySelectorAll('.answer')
+const questionEl = document.getElementById('question')
+const a_text = document.getElementById('a_text')
+const b_text = document.getElementById('b_text')
+const c_text = document.getElementById('c_text')
+const d_text = document.getElementById('d_text')
+const sumbitBtn = document.getElementById('sumbit')
+
+let questionLevel = 1
+let currentQuiz = 0
+let score = 0
+
+loadQuiz()
+
+function loadQuiz() {
+
+    deselectAnswers()
+
+    const currentQuizData = quizData[currentQuiz]
+
+    questionEl.innerText = currentQuizData.question
+    a_text.innerText = currentQuizData.a
+    b_text.innerText = currentQuizData.b
+    c_text.innerText = currentQuizData.c
+    d_text.innerText = currentQuizData.d
+    text.innerHTML = `<p>WYNIK: ${score}/${quizData.length}</p>`
+    quizLevel.innerHTML = `<p>PYTANIE: ${questionLevel}/${quizData.length}</p>`
+}
+
+function deselectAnswers() {
+    answerEls.forEach(answerEl => answerEl.checked = false)
+}
+
+function getSelected() {
+    let answer
+    answerEls.forEach(answerEl => {
+        if(answerEl.checked) {
+            answer = answerEl.id
+        }
+    })
+    return answer
+}
+
+
+sumbitBtn.addEventListener('click', () => {
+    const answer = getSelected()
+    if(answer) {
+       if(answer === quizData[currentQuiz].correct) {
+           score++
+       }
+
+        currentQuiz++
+        questionLevel++
+
+       if(currentQuiz < quizData.length) {
+           loadQuiz()
+       } else {
+           const endTime = new Date().getTime();
+
+           const gameTime = (endTime - startTime) / 1000
+           quiz.innerHTML = `
+           <h2>Odpowiedziałeś ${score} poprawnie na ${quizData.length} zadań.
+           <br>
+           <br>
+           Spędziłeś ${gameTime}sekund!</h2>
+
+           <button onclick="location.reload()">Spróbuj Ponownie</button>
+           `
+       }
+    }
+})
---
+++ script1.js
@@ -0,0 +1,162 @@
+const quizData = [
+    {
+        question: "Jaka firma stworzyła VSC?",
+        a: "Microsoft",
+        b: "Sony",
+        c: "Intel",
+        d: "Inna...",
+        correct: "a",
+    },
+    {
+        question: "Czy rozszerzenie EMMET jest wbudowane w VSC?",
+        a: "Nie ma takiego rozszerzenia",
+        b: "Nie",
+        c: "Tak",
+        d: "Chyba",
+        correct: "c",
+    },
+    {
+        question: "Czy VSC jest darmowym programem?",
+        a: "Nie",
+        b: "Tak",
+        c: "Jest w promocji -100%",
+        d: "Chyba",
+        correct: "b",
+    },
+    {
+        question: "Skąd pobieramy rozszerzenia do VSC?",
+        a: "Z internetu",
+        b: "Z ich strony www",
+        c: "Z innej aplikacji",
+        d: "Z VSC",
+        correct: "d",
+    },
+    {
+        question: "W którym roku powstał VSC?",
+        a: "29 Listopada 2015r",
+        b: "17 Kwietnia 2016r",
+        c: "29 Kwietnia 2015r",
+        d: "17 Stycznia 2014r!",
+        correct: "c",
+    },
+    {
+        question: "Którego z podanych systemów VSC nie wspiera",
+        a: "Windows",
+        b: "Linux",
+        c: "MacOS",
+        d: "Nie podano powyżej!",
+        correct: "d",
+    },
+    {
+        question: "Powiązane z VSC strony to:",
+        a: "VS/Electron/Brackets",
+        b: "Atom/Brackets;",
+        c: "Electron/Brackets/Atom",
+        d: "Nie ma powiązanych stron...",
+        correct: "a",
+    },
+    {
+        question: "Czy w VSC istnieje AutoZapis?",
+        a: "Tak",
+        b: "Nie",
+        c: "Tak ale sam musisz go włączyć",
+        d: "Nie dopuki nie włączysz Strony",
+        correct: "c",
+    },
+    {
+        question: "Aby odświeżać swoją stronę localhost w czasie rzeczywistym, potrzebujesz rozszerzenia:",
+        a: "LiveShare",
+        b: "Auto Rename TAG",
+        c: "LiveServer",
+        d: "Prettier",
+        correct: "c",
+    },
+    {
+        question: "Czy VisualStudio (VS) Powstało wcześniej niż VisualStudioCode (VSC)?",
+        a: "Tak",
+        b: "W tym samym czasie",
+        c: "Nie",
+        d: "Brak poprawnej Odpowiedzi",
+        correct: "a",
+    },
+
+
+];
+
+const startTime = new Date().getTime();
+
+const quiz = document.getElementById('quiz')
+const text = document.getElementById('score')
+const quizLevel = document.getElementById('time')
+const answerEls = document.querySelectorAll('.answer')
+const questionEl = document.getElementById('question')
+const a_text = document.getElementById('a_text')
+const b_text = document.getElementById('b_text')
+const c_text = document.getElementById('c_text')
+const d_text = document.getElementById('d_text')
+const sumbitBtn = document.getElementById('sumbit')
+
+let questionLevel = 1
+let currentQuiz = 0
+let score = 0
+
+loadQuiz()
+
+function loadQuiz() {
+
+    deselectAnswers()
+
+    const currentQuizData = quizData[currentQuiz]
+
+    questionEl.innerText = currentQuizData.question
+    a_text.innerText = currentQuizData.a
+    b_text.innerText = currentQuizData.b
+    c_text.innerText = currentQuizData.c
+    d_text.innerText = currentQuizData.d
+    text.innerHTML = `<p>WYNIK: ${score}/${quizData.length}</p>`
+    quizLevel.innerHTML = `<p>PYTANIE: ${questionLevel}/${quizData.length}</p>`
+    
+}
+
+function deselectAnswers() {
+    answerEls.forEach(answerEl => answerEl.checked = false)
+}
+
+function getSelected() {
+    let answer
+    answerEls.forEach(answerEl => {
+        if(answerEl.checked) {
+            answer = answerEl.id
+        }
+    })
+    return answer
+}
+
+
+sumbitBtn.addEventListener('click', () => {
+    const answer = getSelected()
+    if(answer) {
+       if(answer === quizData[currentQuiz].correct) {
+           score++
+       }
+
+        currentQuiz++
+        questionLevel++
+
+       if(currentQuiz < quizData.length) {
+           loadQuiz()
+       } else {
+           const endTime = new Date().getTime();
+
+           const gameTime = (endTime - startTime) / 1000
+           quiz.innerHTML = `
+           <h2>Odpowiedziałeś ${score} poprawnie na ${quizData.length} zadań.
+           <br>
+           <br>
+           Spędziłeś ${gameTime}sekund!</h2>
+
+           <button onclick="location.reload()">Spróbuj Ponownie</button>
+           `
+       }
+    }
+})
